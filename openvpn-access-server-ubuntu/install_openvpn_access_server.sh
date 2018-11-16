1a08cff6cf9c828a02e462f712bcdbf6825058c3
Kunal Patwardhan <kunal.patwardhan@gmail.com>
2018-11-16 12:43:47 +0530

    Update install_openvpn_access_server.sh
    backup and restore open access server cnfig

--- openvpn-access-server-ubuntu/install_openvpn_access_server.sh
+++ openvpn-access-server-ubuntu/install_openvpn_access_server.sh
@@ -4,6 +4,12 @@ userPassword=$1
 #download the packages
 cd /tmp
 wget -c http://swupdate.openvpn.org/as/openvpn-as-2.1.9-Ubuntu16.amd_64.deb
+#download predefined access serever settings files
+wget -c https://raw.githubusercontent.com/kunalpatwardhan/azure-quickstart-templates/master/openvpn-access-server-ubuntu/as.conf.bak
+wget -c https://raw.githubusercontent.com/kunalpatwardhan/azure-quickstart-templates/master/openvpn-access-server-ubuntu/certs.db.bak
+wget -c https://raw.githubusercontent.com/kunalpatwardhan/azure-quickstart-templates/master/openvpn-access-server-ubuntu/config.db.bak
+wget -c https://raw.githubusercontent.com/kunalpatwardhan/azure-quickstart-templates/master/openvpn-access-server-ubuntu/log.db.bak
+wget -c https://raw.githubusercontent.com/kunalpatwardhan/azure-quickstart-templates/master/openvpn-access-server-ubuntu/userprop.db.bak
 
 #install the software
 sudo dpkg -i openvpn-as-2.1.9-Ubuntu16.amd_64.deb
@@ -14,6 +20,31 @@ sudo echo "openvpn:$userPassword"|sudo chpasswd
 #configure server network settings
 PUBLICIP=$(curl -s ifconfig.me)
 sudo apt-get install sqlite3
+
+sudo su
+#backup existing access server configuraion
+cd /usr/local/openvpn_as/
+./bin/sqlite3 ./etc/db/config.db .dump > ./config.db.bak
+./bin/sqlite3 ./etc/db/certs.db .dump > ./certs.db.bak
+./bin/sqlite3 ./etc/db/userprop.db .dump > ./userprop.db.bak
+./bin/sqlite3 ./etc/db/log.db .dump > ./log.db.bak
+cp ./etc/as.conf ./as.conf.bak
+
+#copy predefined access serever settings files
+service openvpnas stop
+cd /usr/local/openvpn_as/
+rm ./etc/db/config.db
+rm ./etc/db/certs.db
+rm ./etc/db/userprop.db
+rm ./etc/db/log.db
+rm ./etc/as.conf
+./bin/sqlite3 <./config.db.bak ./etc/db/config.db
+./bin/sqlite3 <./certs.db.bak ./etc/db/certs.db
+./bin/sqlite3 <./userprop.db.bak ./etc/db/userprop.db
+./bin/sqlite3 <./log.db.bak ./etc/db/log.db
+cp ./as.conf.bak ./etc/as.conf
+service openvpnas start
+
 sudo sqlite3 "/usr/local/openvpn_as/etc/db/config.db" "update config set value='$PUBLICIP' where name='host.name';"
 
 #restart OpenVPN AS service
